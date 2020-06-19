aa2bab15fa714195e6de99806a7605611dfdd2f6
Solidabhi <abhishekchintamani4@gmail.com>
2020-06-20 02:11:46 +0530

    success

---
+++ Framework/tests/InventoryManagement/Result/InventoryManagement_1/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\91880\\Desktop\\UFT\\Framework\\tests\\InventoryManagement\\Result\\InventoryManagement_1',namePairs:[]}]};
