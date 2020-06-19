c75ff13c80b104e9eff9eb965db6a5bab577cc1f
Solidabhi <abhishekchintamani4@gmail.com>
2020-06-20 02:56:00 +0530

    Pass

---
+++ Framework/tests/CommitAndPush/Result/CommitAndPush_4/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\91880\\Desktop\\UFT\\Framework\\tests\\CommitAndPush\\Result\\CommitAndPush_4',namePairs:[]}]};
